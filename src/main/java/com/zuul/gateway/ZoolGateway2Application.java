4f231b60b5b4ea191e94e6a3140461b51555fed4
rohithsrinivas <rohithsrinivas.94@gmail.com>
2018-04-29 15:38:51 +0530

    adding fall back provider bean

--- src/main/java/com/zuul/gateway/ZoolGateway2Application.java
+++ src/main/java/com/zuul/gateway/ZoolGateway2Application.java
@@ -1,24 +1,109 @@
 package com.zuul.gateway;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
-@ComponentScan(basePackages="com.zuul.*")
+@ComponentScan(basePackages = "com.zuul.*")
+@EnableHystrix
 public class ZoolGateway2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZoolGateway2Application.class, args);	
+		SpringApplication.run(ZoolGateway2Application.class, args);
 	}
-	
-//	@Bean
-//	  public ZuulSimpleFilter simpleFilter() {
-//	    return new ZuulSimpleFilter();
-//	  }
+
+	// @Bean
+	// public ZuulSimpleFilter simpleFilter() {
+	// return new ZuulSimpleFilter();
+	// }
+
+	@Bean
+	public ZuulFallbackProvider zuulFallbackProvider() {
+
+		return new ZuulFallbackProvider() {
+
+			@Override
+
+			public String getRoute() {
+
+				return "zuulRouter";
+
+			}
+
+			@Override
+
+			public ClientHttpResponse fallbackResponse() {
+
+				return new ClientHttpResponse() {
+
+					@Override
+
+					public HttpHeaders getHeaders() {
+						HttpHeaders headers = new HttpHeaders();
+						headers.setContentType(MediaType.APPLICATION_JSON);
+						return headers;
+
+					}
+
+					@Override
+
+					public InputStream getBody() throws IOException {
+
+						return new ByteArrayInputStream("fallback".getBytes());
+
+					}
+
+					@Override
+
+					public String getStatusText() throws IOException {
+
+						return "OK";
+
+					}
+
+					@Override
+
+					public HttpStatus getStatusCode() throws IOException {
+
+						return HttpStatus.OK;
+
+					}
+
+					@Override
+
+					public int getRawStatusCode() throws IOException {
+
+						return 200;
+
+					}
+
+					@Override
+
+					public void close() {
+
+					}
+
+				};
+
+			}
+
+		};
+
+	}
 }
