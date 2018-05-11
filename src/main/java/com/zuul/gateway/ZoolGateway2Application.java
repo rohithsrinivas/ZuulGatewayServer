1d96557f1d62fc139751927905edd34476b01912
rohithsrinivas <rohithsrinivas.94@gmail.com>
2018-05-11 21:21:53 +0530

    adding circuit breaker

--- src/main/java/com/zuul/gateway/ZoolGateway2Application.java
+++ src/main/java/com/zuul/gateway/ZoolGateway2Application.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -21,7 +22,7 @@ import org.springframework.http.client.ClientHttpResponse;
 @EnableZuulProxy
 @EnableEurekaClient
 @ComponentScan(basePackages = "com.zuul.*")
-@EnableHystrix
+@EnableCircuitBreaker
 public class ZoolGateway2Application {
 
 	public static void main(String[] args) {
